58b59372f9d078a07198f013f6a4759f0373f845
Marta <100452234@alumnos.uc3m.es>
2022-11-17 21:11:42 +0100

    CMakeLists without commenting tests

--- cmake-build-release/cmake_install.cmake
+++ cmake-build-release/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/rodri/Documents/GitHub/CAProject2
+# Install script for directory: C:/Users/Marta/Documents/GitHub/CAProject2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,17 +39,17 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/rodri/Documents/GitHub/CAProject2/cmake-build-release/common/cmake_install.cmake")
+  include("C:/Users/Marta/Documents/GitHub/CAProject2/cmake-build-release/common/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/rodri/Documents/GitHub/CAProject2/cmake-build-release/aos/cmake_install.cmake")
+  include("C:/Users/Marta/Documents/GitHub/CAProject2/cmake-build-release/aos/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/rodri/Documents/GitHub/CAProject2/cmake-build-release/soa/cmake_install.cmake")
+  include("C:/Users/Marta/Documents/GitHub/CAProject2/cmake-build-release/soa/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -60,5 +60,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/rodri/Documents/GitHub/CAProject2/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Marta/Documents/GitHub/CAProject2/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- cmake-build-release/common/cmake_install.cmake
+++ cmake-build-release/common/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/rodri/Documents/GitHub/CAProject2/common
+# Install script for directory: C:/Users/Marta/Documents/GitHub/CAProject2/common
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
